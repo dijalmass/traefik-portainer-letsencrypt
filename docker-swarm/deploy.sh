0ab583c93e720be50f239eee8875dcc0a3de3924
Dijalma Silva <dijalmacz@gmail.com>
2025-04-03 18:26:13 -0300

    feat(swarm): add folder with instructions to docker swarm

--- docker-swarm/deploy.sh
+++ docker-swarm/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+set -a
+source .env
+set +a
+
+echo $DOMAIN_NAME $EMAIL_ADDRESS &&\
 docker stack rm traefik-swarm &&\
-docker stack deploy -c docker-compose.yml traefik-swarm
+docker stack deploy -c docker-compose.yml traefik-swarm --detach=false
