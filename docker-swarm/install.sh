a3e00e7c936be0c952c54d8d58e10be9fd7854a9
Dijalma Silva <dijalmacz@gmail.com>
2025-04-03 17:36:09 -0300

    feat(swarm): add folder with instructions to docker swarm

--- docker-swarm/install.sh
+++ docker-swarm/install.sh
@@ -2,8 +2,6 @@
 
 touch /letsencrypt/acme.json &&\
 chmod 600 /letsencrypt/acme.json &&\
-docker swarm init &&\
-docker stack rm traefik-swarm &&\
 docker network rm traefik &&\
 docker network create --driver=overlay traefik &&\
 docker stack deploy -c docker-compose.yml traefik-swarm
